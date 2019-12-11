06b5b88dbeaeea0f666efd2dfa97d29cfe0be5e3
Ilya Qruk <lqrukl@gmail.com>
2019-12-11 07:41:32 -0800

    admin panel changed

--- aggregator/admin.py
+++ aggregator/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Article, Thread
 
-admin.site.register(Article)
-admin.site.register(Thread)
+#admin.site.register(Article)
+#admin.site.register(Thread)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'author', 'thread', 'date')
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'date')
+
