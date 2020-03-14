5a8f5a68e1562926400eb736b9f15ae41352f15a
Ilya Qruk <lqrukl@gmail.com>
2020-03-14 03:36:01 -0700

    Security fix

--- gposerver/settings.py
+++ gposerver/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1yem&vrghg&3t5-4izwun262ll)udq$5rcf+$g-rmgwhh1+#o7'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
